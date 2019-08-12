3fa05c48b95f6a2bc532d617b2e3e57326d534b3
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2019-08-12 18:13:27 +0400

    Some UI improvements

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import Header from '../header';
-import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import Header from "../header";
+import RandomPlanet from "../random-planet";
+import ItemList from "../item-list";
+import PersonDetails from "../person-details";
 
-import './app.css';
+import "./app.css";
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <Header />
       <RandomPlanet />
 
